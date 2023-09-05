d281a70f3dc858a95f50c694d58c64eb30945a60
TylerHumb <101156725+TylerHumb@users.noreply.github.com>
2023-09-05 11:55:35 +1000

    Created

---
+++ src/test/java/org/example/EventTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+    Event testevent;
+    @BeforeEach
+    void Setup(){
+
+    }
+
+
+    @Test
+    void AdminLoginTest() {
+        testevent.AdminLogin();
+    }
+}
