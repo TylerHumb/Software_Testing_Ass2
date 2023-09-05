9e1954919d5df09fcd0c8ea3d79e034eef8206f9
TylerHumb <S3947682@student.rmit.edu.au>
2023-09-05 16:32:53 +1000

    Added Spiraplan support

--- src/test/java/org/example/EventTest.java
+++ src/test/java/org/example/EventTest.java
@@ -8,7 +8,23 @@ import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import com.inflectra.spiratest.addons.junitextension.SpiraTestCase;
+import com.inflectra.spiratest.addons.junitextension.SpiraTestConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpiraTestConfiguration(
+        //following are REQUIRED
+        url = "https://rmit.spiraservice.net",
+        login = "S3947682",
+        rssToken = "{DA4EA898-6C4C-4253-9D7A-8733A126142B}",
+        projectId = 78
+
+)
 class EventTest {
     Event testevent;
     Scanner scnr= Mockito.mock(Scanner.class);
@@ -17,46 +33,48 @@ class EventTest {
         testevent = new Event(scnr);
     }
     @Test
-    void ValidAdminLoginTest() {
+    @SpiraTestCase(testCaseId = 1373)
+    public void ValidAdminLoginTest() {
         Mockito.when(scnr.nextLine()).thenReturn("Admin1").thenReturn("pass1");
         assertTrue(testevent.AdminLogin());
     }
     @Test
-    void InvalidAdminLoginTest() {
+    @SpiraTestCase(testCaseId = 375)
+    public void InvalidAdminLoginTest() {
         Mockito.when(scnr.nextLine()).thenReturn("wrong").thenReturn("pass1");
         assertFalse(testevent.AdminLogin());
     }
     @Test
-    void ValidStudentLoginTest() {
+    public void ValidStudentLoginTest() {
         // they used a very annoying way of collecting user input here
         Mockito.when(scnr.nextInt()).thenReturn(7654324);
         Mockito.when(scnr.next()).thenReturn("p7654324#");
         assertTrue(testevent.StudentLogin());
     }
     @Test
-    void InvalidStudentLoginTest() {
+    public void InvalidStudentLoginTest() {
         Mockito.when(scnr.nextInt()).thenReturn(10);
         Mockito.when(scnr.next()).thenReturn("testers");
         assertFalse(testevent.StudentLogin());
     }
     @Test
-    void ViewDetailsTest(){
+    public void ViewDetailsTest(){
         assertTrue(testevent.viewStudentDetails());
     }
     @Test
-    void ValidSearchDetailsTest(){
+    public void ValidSearchDetailsTest(){
         assertTrue(testevent.searchStudentDetails(7654324));
     }
     @Test
-    void InvalidSearchDetailsTest(){
+    public void InvalidSearchDetailsTest(){
         assertFalse(testevent.searchStudentDetails(102030));
     }
     @Test
-    void StudentCountTest(){
+    public void StudentCountTest(){
         assertEquals(testevent.countStudent(),9);
     }
     @Test
-    void ValidRemoveStudentTest(){
+    public void ValidRemoveStudentTest(){
         int studentcount = testevent.countStudent();
         //need to spy the method in order to skip the writing to student file,
         // as to not affect future tests and alter the record during testing
@@ -69,11 +87,11 @@ class EventTest {
         );
     }
     @Test
-    void InvalidRemoveStudentTest(){
+    public void InvalidRemoveStudentTest(){
         assertFalse(testevent.removeStudent(299332));
     }
     @Test
-    void  ValidAddStudentTest(){
+    public void  ValidAddStudentTest(){
         //need to spy the method in order to skip the writing to student file,
         // as to not affect future tests and alter the record during testing
         Event fakeevent = spy(new Event(scnr));
@@ -83,6 +101,4 @@ class EventTest {
         Mockito.when(scnr.nextLine()).thenReturn("p2222229#");
         assertEquals("Student Added Successfully",fakeevent.AddStudent());
     }
-    @Test
-    void
 }
